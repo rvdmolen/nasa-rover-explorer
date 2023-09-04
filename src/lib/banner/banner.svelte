44cef8cfbe9e69821a17cc0ce00695f906f3cffc
Richard vd Molen <rvdmolen@users.noreply.github.com>
2023-09-04 18:10:21 +0200

    Feat/upgrade latest (#2)
    * upgrade
    
    * version bump
    
    * upgrade latest
    
    * fix the build
    
    * version bump
    
    ---------
    
    Co-authored-by: isc90371 <richard.van.der.molen@politie.nl>

--- src/lib/banner/banner.svelte
+++ src/lib/banner/banner.svelte
@@ -1,10 +1,23 @@
+<script>
+  import space8 from '$lib/images/space8.svg';
+</script>
+
 <style>
   .banner {
     min-height: 150px;
-		max-height: 200px;
-		background-size: cover;
-		background-position: top top;
-	}
+    max-height: 200px;
+    position: relative;
+  }
+
+  .banner img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+  }
 </style>
 
-<div class="shadow-2xl banner bg-[url('space8.svg')]" />
+<div class="shadow-2xl banner">
+  <img src={space8} alt="banner" />
+</div>
+
+
